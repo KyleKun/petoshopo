27eb69890bd6a3bf776999877769f94578184fa6
samuel <samuel.santos51@fatec.sp.gov.br>
2019-12-03 17:20:00 -0300

    testando

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(Petoshopo());
-
+//N leia essa msg
 class Petoshopo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
